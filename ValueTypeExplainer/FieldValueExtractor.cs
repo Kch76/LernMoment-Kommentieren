184e0d383f12c61a25d1cd2acef8c1e4de41649d
Jan (LernMoment.de) <jan@lernmoment.de>
2019-09-01 12:42:44 +0200

    Überführt Kommentare in Methoden
    Kommentare können irgendwann nicht mehr zum Quellcode passen. Also versuche ich den Quellcode so zu schreiben, dass er keinen Kommentar benötigt.

--- ValueTypeExplainer/FieldValueExtractor.cs
+++ ValueTypeExplainer/FieldValueExtractor.cs
@@ -21,26 +21,12 @@ namespace ValueTypeExplainer
         public T GetFieldInfoForValueType(string fieldName)
         {
             T result;
-            object tmpResult;
 
-            // Überprüfen ob der Datentyp (T) dieser Instanz tatsächlich ein ValueType ist.
-            if (aValue.GetType().IsValueType)
+            if (IsValueType())
             {
-                // Überprüfen ob fieldName für diesen Datentyp gültig ist bzw. definiert ist
-                if (fieldName?.Length > 0 && aValue.GetType().GetFields().Where(f => f.Name == fieldName).FirstOrDefault() != null)
+                if (IsValidFieldNameForThisDataType(fieldName))
                 {
-                    // Den Wert für fieldName "beschaffen"
-                    tmpResult = aValue.GetType().GetField(fieldName).GetValue(aValue.GetType().GetField(fieldName));
-
-                    // Können wir den Wert aus dieser Methode zurück geben?
-                    if (tmpResult.GetType() == typeof(T))
-                    {
-                        result = (T)tmpResult;
-                    }
-                    else
-                    {
-                        throw new Exception(string.Format("Typ = {0} passt nicht zum Rückgabewert des geforderten Feldes {1} vom Typ {2}.", typeof(T), fieldName, tmpResult.GetType()));
-                    }
+                    result = GetValueOfFieldName(fieldName);
                 }
                 else
                 {
@@ -53,6 +39,39 @@ namespace ValueTypeExplainer
             }
 
             return result;
+        }
+
+        private T GetValueOfFieldName(string fieldName)
+        {
+            T result;
+            object tmpResult = GetValue(fieldName);
+
+            // Können wir den Wert aus dieser Methode zurück geben?
+            if (tmpResult.GetType() == typeof(T))
+            {
+                result = (T)tmpResult;
+            }
+            else
+            {
+                throw new Exception(string.Format("Typ = {0} passt nicht zum Rückgabewert des geforderten Feldes {1} vom Typ {2}.", typeof(T), fieldName, tmpResult.GetType()));
+            }
+
+            return result;
+        }
+
+        private object GetValue(string fieldName)
+        {
+            return aValue.GetType().GetField(fieldName).GetValue(aValue.GetType().GetField(fieldName));
+        }
+
+        private bool IsValidFieldNameForThisDataType(string fieldName)
+        {
+            return fieldName?.Length > 0 && aValue.GetType().GetFields().Where(f => f.Name == fieldName).FirstOrDefault() != null;
+        }
+
+        private bool IsValueType()
+        {
+            return aValue.GetType().IsValueType;
         }
     }
 }
